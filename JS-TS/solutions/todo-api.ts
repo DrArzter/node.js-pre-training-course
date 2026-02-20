3a47f796b4c79a492976aef65d694db635d51f5d
DrArzter <chapegarostislav@gmail.com>
2026-02-20 23:46:41 +0100

    JS-TS-task-06: implement todo API methods: getAll, add, update, and remove with error handling with in-memory storage

--- JS-TS/solutions/todo-api.ts
+++ JS-TS/solutions/todo-api.ts
@@ -1,22 +1,49 @@
-import { InMemoryRepository } from './repository';
 import { Todo, NewTodo } from './types';
+import { createTodo } from './todo-factory';
+
+export class TodoNotFoundError extends Error {
+  constructor(id: number) {
+    super(`Todo with id ${id} not found`);
+    this.name = 'TodoNotFoundError';
+  }
+}
 
 export class TodoApi {
-  private repo = new InMemoryRepository<Todo>();
+  private todos: Todo[] = [];
+
+  private randomDelay(): Promise<void> {
+    const ms = 300 + Math.floor(Math.random() * 300);
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
   async getAll(): Promise<Todo[]> {
-    throw new Error('getAll: not implemented');
+    await this.randomDelay();
+    return [...this.todos];
   }
 
   async add(newTodo: NewTodo): Promise<Todo> {
-    throw new Error('add: not implemented');
+    await this.randomDelay();
+    const todo = createTodo(newTodo);
+    this.todos.push(todo);
+    return todo;
   }
 
   async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
-    throw new Error('update: not implemented');
+    await this.randomDelay();
+    const index = this.todos.findIndex(t => t.id === id);
+    if (index === -1) {
+      throw new TodoNotFoundError(id);
+    }
+    this.todos[index] = { ...this.todos[index], ...update };
+    return this.todos[index];
   }
 
   async remove(id: number): Promise<void> {
-    throw new Error('remove: not implemented');
+    await this.randomDelay();
+    const index = this.todos.findIndex(t => t.id === id);
+    if (index === -1) {
+      throw new TodoNotFoundError(id);
+    }
+    this.todos.splice(index, 1);
   }
 }
